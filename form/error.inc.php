42157c3a9e899088fc31806b99ca1bb3077cf47c
Nasim Eghlima <neghlima@ufl.edu>
2021-04-25 19:06:04 -0700

    added extra pages for form

---
+++ form/error.inc.php
@@ -0,0 +1,150 @@
+<style type="text/css">
+*{
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+header {
+  background: white;
+  position: relative;
+  width: 100%;
+  z-index: 99999;
+}
+
+header nav ul li {
+  display: inline-block;
+  padding: 0 15px;
+  font-family: "lora", sans-serif;
+}
+
+header nav a:hover{
+  background-color: #36008d6e;
+  transition: .3s;
+}
+
+header nav a {
+  padding: 15px;
+  text-decoration: none;
+  background: #85f3d999;
+  color: black;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 22px;
+  border-radius: 5px;
+  transition: .3s;
+  }
+
+  .logo-container {
+    width: 30%;
+    display: inline-block;
+}
+
+header nav {
+    width: 68%;
+    display: inline-block;
+    text-align: right;
+    vertical-align: top;
+    margin-top: 50px;
+}
+
+.logo-container,
+header nav {
+
+  vertical-align: right;
+}
+
+.container {
+  max-width: 900px;
+  margin: 60px auto 180px;
+  text-align: center;
+  padding: 40px;
+}
+footer a:hover {
+  color: white;
+  transition: .3s;
+}
+
+
+.footer-icon {
+  display: inline-block;
+  width: 19%;
+  font-size: 25px;
+}
+
+.footer-icon a {
+  font-size: 36px;
+}
+
+footer nav {
+    width: 80%;
+    display: inline-block;
+    text-align: right;
+}
+footer nav a {
+  color: white;
+  transition: .3s;
+}
+
+footer nav a:hover {
+  color: #85f3d999;
+  transition: .3s;
+}
+
+footer nav ul li {
+  display: inline-block;
+  padding: 0 15px;
+  font-family: "arimo", sans-serif;
+}
+
+
+</style>
+
+<header>
+
+  <div class="logo-container">
+    <a href="=index.html">
+      <img src= 'image/logo_t.png' alt="themintedgirl Logo" width="180" />
+    </a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="about.html">About Me</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+	<h1>Missing fields</h1>
+	<p>Sorry, you have not completed all of the required fields.</p>
+	<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
+
+	<ul>
+	<?php
+		for($i=0; $i<count($this->missing_required_fields); $i++){
+			echo "<li>" . $this->missing_required_fields[$i]['title'] . "</li>\n";
+		}
+	?>
+	</ul>
+
+	<p><strong><a href="index.html" >Back to form</a></strong></p>
+</div>
+
+<footer>
+  <section class="footer-icon">
+    <a href="https://www.facebook.com/The-Minted-Girl-110144481157024" target="_blank"><i class="fab fa-facebook"></i></a>
+    <a href="https://www.instagram.com/themintedgirl/" target="_blank"><i class="fab fa-instagram"></i></a>
+  </section>
+
+   <nav>
+     <ul class="footer-nav">
+       <li><a href="home.html">Home</a></li>
+       <li><a href="contact.html">Contact Us</a></li>
+       <li><a href="#top">Back to Top ^ </a></li>
+     </ul>
+   </nav>
+
+</footer>
